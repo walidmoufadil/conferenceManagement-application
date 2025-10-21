420ba7c2566faf0a07cc9b6e0e0bb3178d46a8e3
MOUFADIL walid <walid.moufadil-etu@etu.univh2c.ma>
2025-10-21 01:18:40 +0100

    update keynote service

--- keynote-service/src/main/java/org/example/keynoteservice/config/RestConfig.java
+++ keynote-service/src/main/java/org/example/keynoteservice/config/RestConfig.java
@@ -1,4 +1,15 @@
 package org.example.keynoteservice.config;
 
-public class RestConfig {
+import org.example.keynoteservice.entity.Keynote;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class RestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(Keynote.class);
+    }
 }
--- keynote-service/src/main/java/org/example/keynoteservice/model/Conference.java
+++ keynote-service/src/main/java/org/example/keynoteservice/model/Conference.java
@@ -1,4 +1,19 @@
 package org.example.keynoteservice.model;
 
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
 public class Conference {
+
+    private Long id;
+    private String titre;
+    private String type;
+    private Date date;
+    private Double duree;
+    private int nombreInscrits;
+    private Double score;
+    private Long keynoteId;
 }
+
