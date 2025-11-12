7fffbd9bc9f02af96803617d6c5256058a2e50b6
MOUFADIL walid <walid.moufadil-etu@etu.univh2c.ma>
2025-11-12 16:17:43 +0100

    update repository package

--- keynote-service/src/main/java/org/example/keynoteservice/repository/KeynoteRepository.java
+++ keynote-service/src/main/java/org/example/keynoteservice/repository/KeynoteRepository.java
@@ -2,8 +2,6 @@ package org.example.keynoteservice.repository;
 
 import org.example.keynoteservice.entity.Keynote;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
 public interface KeynoteRepository extends JpaRepository<Keynote, Long> {
 }
