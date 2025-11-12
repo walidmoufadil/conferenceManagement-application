a77c91a70d842803949b0c34996cc630331871e2
MOUFADIL walid <walid.moufadil-etu@etu.univh2c.ma>
2025-11-12 16:16:35 +0100

    update dto package

--- keynote-service/src/main/java/org/example/keynoteservice/dto/KeynoteRequestDTO.java
+++ keynote-service/src/main/java/org/example/keynoteservice/dto/KeynoteRequestDTO.java
@@ -1,4 +1,17 @@
 package org.example.keynoteservice.dto;
 
+import lombok.*;
+import org.example.keynoteservice.entity.Keynote;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
 public class KeynoteRequestDTO {
+    private String nom;
+    private String prenom;
+    private String email;
+    private String fonction;
+
 }
--- keynote-service/src/main/java/org/example/keynoteservice/dto/KeynoteResponseDTO.java
+++ keynote-service/src/main/java/org/example/keynoteservice/dto/KeynoteResponseDTO.java
@@ -1,4 +1,17 @@
 package org.example.keynoteservice.dto;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
 public class KeynoteResponseDTO {
+    private Long id;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String fonction;
+
 }
